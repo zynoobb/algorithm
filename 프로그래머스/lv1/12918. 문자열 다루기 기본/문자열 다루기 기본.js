d79248f44f5b4f12b1466a1a9d56508c325f5df1
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-01-19 12:19:23 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,12 @@
+function solution(s) {
+      if(s.length !== 4 && s.length !== 6){
+    return false
+  }
+  for(let i = 0 ; i< s.length ; i++){
+    if(Number.isNaN(Number(s[i])) == true){
+      return false
+    }
+  }
+  return true
+  
+}
