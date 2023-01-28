05b38f44234ccf0284473d1b4d4cd9f6ace5a6e5
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-01-28 14:24:17 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,9 @@
+function solution(numbers, direction) {
+    if(direction === "right"){
+      numbers.unshift(numbers.pop())
+    }else {
+      numbers.push(numbers.shift())
+    }
+  
+  return numbers
+}
