abb6203614fa6b86b000d87d1d8f16d24e621dfb
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-01-29 16:45:12 +0900

    [level 1] Title: 모의고사, Time: 3.56 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,26 +1,29 @@
 function solution(answers) {
-      let a1 = [1,2,3,4,5,1,2,3,4,5]
-  let a2 = [2, 1, 2, 3, 2, 4, 2, 5]
+let a1 = [1,2,3,4,5]
+  let a2 = [2,1,2,3,2,4,2,5]
   let a3 = [3,3,1,1,2,2,4,4,5,5]
-      let temp = []
-  const a11 = answers.filter((el,i) => 
-   el === a1[i % (a1.length)]
-  )
-  const a12 = answers.filter((el,i) => 
-   el === a2[i % (a2.length)]
-  )
-  const a13 = answers.filter((el,i) => 
-   el === a3[i % (a3.length)]
-  )
-  temp = [a11.length,a12.length,a13.length]
   
-  const result = Math.max(...temp)
-  
-  let aaa = []
-  for(let i=0; i<temp.length; i++){
-    if(temp[i] === result){
-      aaa.push(i+1)
-    }
+  while (answers.length > a1.length){
+    a1 = a1.concat(a1)
+    a2 = a2.concat(a2)
+    a3 = a3.concat(a3)  
   }
-  return aaa
+  let sum1 = 0
+  let sum2 = 0
+  let sum3 = 0
+  
+  for(let i = 0 ; i < answers.length ;i++){
+    if(a1[i] === answers[i]) sum1 ++
+    if(a2[i] === answers[i]) sum2 ++
+    if(a3[i] === answers[i]) sum3 ++
+  }
+  
+  let max = Math.max(sum1, sum2, sum3)
+  let result = []
+  
+  if(max === sum1) result.push(1)
+  if(max === sum2) result.push(2)
+  if(max === sum3) result.push(3)
+  return result
+
 }
