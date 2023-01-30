1a4948a02fc05f3e864e0e7b51b087d1566e5304
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-01-30 13:46:08 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -3,7 +3,7 @@ let arr = [...s.toLowerCase()]
   let result = ''
   
   for(let i = 0 ; i < arr.length ; i++){
-    if(arr[i-1] == " " || arr[i-1] == undefined){
+    if(arr[i-1] === " " || arr[i-1] === undefined){
       result += arr[i].toUpperCase()
     } else {
       result += arr[i]
