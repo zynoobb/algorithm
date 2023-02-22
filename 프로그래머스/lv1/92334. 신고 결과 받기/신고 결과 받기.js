18a527b792622eefd5aa324597eaccd04efbe257
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-02-22 10:37:45 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -2,14 +2,13 @@ function solution(id_list, report, k) {
   report = [...new Set(report)]
   
   let obj = {}
-  let temp = new Set()
+  let stopped = []
   
   for(let i = 0 ; i < report.length ; i ++){
     obj[report[i].split(" ")[1]] = (obj[report[i].split(" ")[1]]|| 0) + 1
-    if(obj[report[i].split(" ")[1]] >= k) temp.add(report[i].split(" ")[1])
+    if(obj[report[i].split(" ")[1]] >= k) stopped.push(report[i].split(" ")[1])
   }
   
-  let stopped = [...temp]
   let result = new Array(id_list.length).fill(0)
   let list = report.filter((a)=> stopped.includes(a.split(" ")[1])).map((b)=> b = b.split(" ")[0])  
   
