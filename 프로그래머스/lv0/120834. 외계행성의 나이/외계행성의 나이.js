827cfc3ec646ecdb10cbd1391c56149aeaf722a1
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-02-28 12:54:44 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -2,7 +2,6 @@ function solution(age) {
 let alp = ['a','b','c','d','e','f','g','h','i','j']
     let result = ''
     let str = String(age)
-    console.log(str)
       
     for(let i = 0 ; i < str.length; i++){
       result +=alp[str[i]]
