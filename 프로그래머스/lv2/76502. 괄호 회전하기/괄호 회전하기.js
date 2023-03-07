ea47f32cb93a33e3dcad03a8575747b65080be53
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-03-07 11:07:47 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,28 +1,38 @@
-
-const numbering = {
-  "[": 0,
-  "]": 1,
-  "{": 2,
-  "}": 3,
-  "(": 4,
-  ")": 5,
-}
-
 function solution(s) {
-  let result = 0
-
-  for (let i = 0; i < s.length; i++) {
-    s = s.slice(1) + s[0]
-    const stack = []
-    for (let j = 0; j < s.length; j++) {
-      if (numbering[s[j]] % 2 === 0) {
-        stack.push(numbering[s[j]])
-      } else if (stack[stack.length - 1] === numbering[s[j]] - 1) {
-        stack.splice(stack.length - 1, 1)
-      } else break
-
-      if (j === s.length - 1 && stack.length == 0) result++
+  const str = "[](){}"
+  const w = (x) => s.indexOf(x) === 0 ? 0.5 : s.indexOf(x)
+  
+  let big = 0
+  let small = 0
+  let mid = 0
+  
+  for(let i = 0 ; i < s.length ; i++){
+    if(s[i] === str[0]) {
+      big ++
+    }else if(s[i] === str[1]){
+      big --
+    }else if(s[i] === str[2]){
+      small ++
+    }else if(s[i] === str[3]){
+      small --
+    }else if(s[i] === str[4]){
+      mid ++
+    }else if(s[i] === str[5]){
+      mid --
     }
   }
+  if(big !== 0 || small !== 0 || mid !== 0) return 0
+  
+  let result = 0 
+  
+  for(let i = 0 ; i < s.length ; i ++){
+    if(
+      w(str[0]) < w(str[1]) 
+      && w(str[2]) < w(str[3]) 
+      && w(str[4]) < w(str[5])
+    ) result ++  
+    s = s.slice(1) + s[0]
+  }
+  
   return result
 }
