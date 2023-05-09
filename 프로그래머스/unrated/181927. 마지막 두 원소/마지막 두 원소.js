7ce460a413e965e710e2f6ff20329aaf537c4f4f
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-05-10 04:39:44 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,7 @@
+function solution(num_list) {
+  num_list.at(-1) > num_list.at(-2) 
+    ? num_list.push(num_list.at(-1)-num_list.at(-2))
+    : num_list.push(num_list.at(-1)*2)
+  
+  return num_list
+}
