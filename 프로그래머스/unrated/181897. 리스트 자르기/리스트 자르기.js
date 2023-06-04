9c25ea9cca4470bc6a6a75d877b163b945061410
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-06-05 02:01:28 +0900

    [unrated] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,7 @@
+function solution(n, slicer, num_list) {
+    const [a,b,c] = slicer
+    if(n === 1) return num_list.slice(0,b+1)
+    if(n === 2) return num_list.slice(a)
+    if(n === 3) return num_list.slice(a,b+1)
+    if(n === 4) return num_list.slice(a,b+1).filter((el,idx)=> idx%c === 0)
+}
