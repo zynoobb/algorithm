9ecca2d7f1a0271322ff332c1fd6a4590cc65d76
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-07-15 19:36:45 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,7 @@
+function solution(numbers) {
+    const result = numbers.map((el)=> String(el))
+        .sort((a,b)=> (b+a)-(a+b))
+        .join("")
+    
+    return result[0] === "0" ? "0" : result
+}
