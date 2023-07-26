7d49c2796e010980aa231afa1f798c57df281381
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-07-27 01:29:24 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2184 ms, Memory: 170076 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+// const input = fs.readFileSync('입력/temp.txt').toString().trim().split('\n')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+input.shift()
+
+function solution(data) {
+  const result = data.sort((a,b)=> Number(a) - Number(b))
+  console.log(result.join("\n"))
+}
+
+solution(input)
