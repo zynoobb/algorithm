9bbd0a8fc7e37e42dc9242d5408c04e57224a673
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-07-26 18:37:55 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 160 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+// const input = fs.readFileSync('입력/temp.txt').toString().trim()
+const input = fs.readFileSync('/dev/stdin').toString().trim()
+
+function solution(data) {
+  const init = "int"
+  const times = data/4
+
+  console.log("long ".repeat(times) + init)
+}
+
+solution(input)
