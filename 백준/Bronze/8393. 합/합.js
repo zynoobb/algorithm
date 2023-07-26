27a7ba9292768ed7b95b438522fbc615615687d2
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-07-26 18:28:00 +0900

    [Bronze V] Title: 합, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+// const input = fs.readFileSync('입력/temp.txt').toString().trim()
+const input = fs.readFileSync('/dev/stdin').toString().trim()
+
+function solution(data) {
+  console.log((data/2)*(Number(data)+1))
+}
+
+solution(input)
