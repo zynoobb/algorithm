04d905fe31112191a29bd649cfaa2e9ef360fc1c
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-07-26 18:49:00 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+// const input = fs.readFileSync('입력/temp.txt').toString().trim().split("\n")
+const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n")
+
+
+function solution(data) {
+  data.forEach((el)=> {
+    const [a,b] = el.split(" ").map((el)=> Number(el))
+    if(a+b !== 0) console.log(a+b)
+  })
+}
+
+solution(input)
