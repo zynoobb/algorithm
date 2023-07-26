d92b29913bcdee5775fbad9c8f4793c109f8467c
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-07-26 18:53:16 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 124 ms, Memory: 9532 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+// const input = fs.readFileSync('입력/temp.txt').toString().trim()
+const input = fs.readFileSync('/dev/stdin').toString().trim()
+
+
+function solution(data) {
+  for(let i = 1 ; i <= data ; i++) {
+    console.log(" ".repeat(data - i) + "*".repeat(i))
+  }
+}
+
+solution(input)
