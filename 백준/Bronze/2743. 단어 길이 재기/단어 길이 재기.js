d71fc8d94c0edeedd55c13c564f22696fdc076ae
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-08-01 04:53:01 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+// const input = fs.readFileSync('입력/temp.txt').toString().trim()
+const input = fs.readFileSync('/dev/stdin').toString().trim()
+
+function solution(data) {
+  console.log(data.length)
+}
+solution(input)
