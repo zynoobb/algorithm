d3c481ba9654b978d5c952e3e63794054e141e08
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-08-02 04:05:56 +0900

    [Bronze IV] Title: 기찍 N, Time: 268 ms, Memory: 23836 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const input = fs.readFileSync(process.platform === "linux" ? "/dev/stdin":"입력.txt")
+  .toString().trim()
+
+function solution(data) {
+  let result = []
+  for(let i = data ; i > 0 ; i --) 
+    result.push(i)
+  
+  console.log(result.join("\n"))
+}
+solution(input)
