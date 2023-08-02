64d9a4e4b8fc2c52408ef23aaea8b4f9be7020f3
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-08-02 19:26:29 +0900

    [Bronze II] Title: 수 정렬하기, Time: 152 ms, Memory: 9396 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const input = fs.readFileSync(process.platform === "linux" ? "/dev/stdin":"입력.txt")
+  .toString().trim().split("\n")
+
+input.shift()
+
+function solution(data) {
+  const result = data.sort((a,b)=> a-b)
+  console.log(result.join('\n'))
+}
+
+solution(input)
