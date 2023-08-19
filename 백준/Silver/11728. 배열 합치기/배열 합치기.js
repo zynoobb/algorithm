712df8ccfdd8e1313091c2334153a1b309452f5c
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-08-20 03:56:38 +0900

    [Silver V] Title: 배열 합치기, Time: 2036 ms, Memory: 434012 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.js
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.js
@@ -1,11 +1,41 @@
 const fs = require('fs')
 const input = fs.readFileSync(process.platform === "linux" ? "/dev/stdin":"입력.txt")
-  .toString().trim().split('\n')
+  .toString().trim().split('\n').map(el=>el.split(" ").map(Number))
 
-input.shift()
+const [aLength,bLength] = input.shift()
 function solution(data) {
-  const [a,b] = data.map(el=>el.split(' ').map(Number))
-  const result = [...a, ...b].sort((x,y)=> x-y).join(" ")
-  console.log(result)
+  const [A,B] = data
+  const result = []
+  let [aIdx, bIdx] = [0,0]
+
+  const lowerCaseAct = (x) => {
+    if(x === "a") {
+      result.push(A[aIdx])
+      aIdx ++
+    } else {
+      result.push(B[bIdx])
+      bIdx ++
+    }
+  }
+
+  const restCaseAct = (x) => {
+    if(x === "a") {
+      result.push(...A.slice(aIdx))
+      aIdx = aLength
+    } else {
+      result.push(...B.slice(bIdx))
+      bIdx = bLength
+    }
+  }
+
+  while (aIdx < aLength || bIdx < bLength) {
+    if(aIdx < aLength && bIdx < bLength) {
+      A[aIdx] < B[bIdx] ? lowerCaseAct("a") : lowerCaseAct("b")
+    } else {
+      aIdx < aLength ? restCaseAct("a") : restCaseAct("b")
+    }
+  }
+
+  console.log(result.join(' '))
 }
 solution(input)
