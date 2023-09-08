0a8c2d0ee5fc81efb62366ad73091f995030b036
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-09-08 14:55:18 +0900

    [Bronze IV] Title: 히스토그램, Time: 116 ms, Memory: 9360 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const input = fs.readFileSync(process.platform === "linux" ? "/dev/stdin":"입력.txt")
+  .toString().trim().split('\n')
+
+function solution(data) {
+  const result = data.slice(1).map((el)=> '='.repeat(el))
+  console.log(result.join('\n'))
+}
+
+solution(input)
