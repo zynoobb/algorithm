c25bb5f21e19c97a62d61420f39b5d96807e1409
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-09-15 12:14:38 +0900

    [Bronze III] Title: 2루수 이름이 뭐야, Time: 116 ms, Memory: 9476 KB -BaekjoonHub

---
+++ 백준/Bronze/17350. 2루수 이름이 뭐야/2루수 이름이 뭐야.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const input = fs.readFileSync(process.platform === "linux" ? "/dev/stdin":"입력.txt")
+  .toString().trim().split('\n')
+
+  function solution(data) {
+    data.shift()
+
+    console.log(data.includes('anj') ? '뭐야;' : '뭐야?')
+  }
+
+  solution(input)
