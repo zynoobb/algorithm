47053aacea586962cdc8c46a252b2fcf655e61f9
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-10-01 13:16:18 +0900

    [Silver V] Title: 창문 닫기, Time: 124 ms, Memory: 9576 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const input = fs.readFileSync(process.platform === "linux" ? "/dev/stdin":"입력.txt")
+  .toString().trim()
+
+function solution(data) {
+  console.log(~~Math.sqrt(data))
+}
+
+solution(input)  
