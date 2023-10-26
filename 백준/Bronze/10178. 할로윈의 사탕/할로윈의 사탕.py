38e2e716a13ec7bdd5998bb527e7eb470be2f128
이진호 <117588482+zynoobb@users.noreply.github.com>
2023-10-26 15:30:32 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.py
@@ -0,0 +1,16 @@
+import sys
+data_temp = sys.stdin if sys.platform == 'linux' else open('입력.txt', 'r')
+input_data = data_temp.read().splitlines()
+
+def solution (data) :
+  data.pop(0)
+  
+  result = list(map(lambda x : act(x), data))
+  print('\n'.join(result))
+  
+def act (_str) :
+  a,b = map(int, _str.split())
+  return 'You get {} piece(s) and your dad gets {} piece(s).'.format(a//b, a%b)
+      
+solution(input_data)
+
