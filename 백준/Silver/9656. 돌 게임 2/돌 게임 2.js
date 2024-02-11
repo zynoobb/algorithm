baba63b6c3618c079204b229861daac91e0084ce
이진호 <117588482+zynoobb@users.noreply.github.com>
2024-02-11 15:03:34 +0900

    [Silver IV] Title: 돌 게임 2, Time: 192 ms, Memory: 9396 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "입력.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+function solution(data) {
+  const N = +data[0];
+
+  const dp = [null, 1, 0, 1];
+
+  for (let i = 4; i <= N; i++) {
+    dp.push(dp[i - 3] + dp[i - 1] === 2 ? 0 : 1);
+  }
+
+  console.log(dp[N] ? "CY" : "SK");
+}
+
+solution(input);
