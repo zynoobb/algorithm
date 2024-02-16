83d92f734f669cb601d77119f16ca651432d53ee
이진호 <117588482+zynoobb@users.noreply.github.com>
2024-02-16 15:42:16 +0900

    [Silver III] Title: 돌 게임 5, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "입력.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+function solution(data) {
+  const n = +data[0];
+
+  console.log(n % 2 === 0 ? "CY" : "SK");
+}
+
+solution(input);
