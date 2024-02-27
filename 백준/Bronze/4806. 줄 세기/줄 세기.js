2eac6cd395c330aee0dc03c9a1aec73a54ca61b0
이진호 <117588482+zynoobb@users.noreply.github.com>
2024-02-27 19:32:23 +0900

    [Bronze III] Title: 줄 세기, Time: 116 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/4806. 줄 세기/줄 세기.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "입력.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+function solution(data) {
+  console.log(data.filter((el) => el !== "").length);
+}
+
+solution(input);
