47cbf93f7bb8d27f9bcaf1994fd455899dee7d7d
이진호 <117588482+zynoobb@users.noreply.github.com>
2024-03-16 16:09:10 +0900

    [Bronze II] Title: 9진수, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/14491. 9진수/9진수.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "입력.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+function solution(data) {
+  const n = +data.shift();
+
+  console.log(n.toString(9));
+}
+
+solution(input);
