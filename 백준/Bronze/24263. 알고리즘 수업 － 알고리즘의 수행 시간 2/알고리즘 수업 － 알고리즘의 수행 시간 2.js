11f2982faa9b5443cf889cb7b0e5728c8bd8225c
이진호 <117588482+zynoobb@users.noreply.github.com>
2024-04-08 13:32:30 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 140 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "입력.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+function solution(data) {
+  const n = +data[0];
+
+  console.log(n);
+  console.log(1);
+}
+
+solution(input);
