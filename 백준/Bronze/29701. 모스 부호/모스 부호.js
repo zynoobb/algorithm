77bb723f8dd637a7ce78214968d0982d70df60f6
이진호 <117588482+zynoobb@users.noreply.github.com>
2024-12-13 22:27:47 +0900

    [Bronze II] Title: 모스 부호, Time: 92 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/29701. 모스 부호/모스 부호.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "입력.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+function solution(data) {
+  const obj = {
+    A: ".-",
+    B: "-...",
+    C: "-.-.",
+    D: "-..",
+    E: ".",
+    F: "..-.",
+    G: "--.",
+    H: "....",
+    I: "..",
+    J: ".---",
+    K: "-.-",
+    L: ".-..",
+    M: "--",
+    N: "-.",
+    O: "---",
+    P: ".--.",
+    Q: "--.-",
+    R: ".-.",
+    S: "...",
+    T: "-",
+    U: "..-",
+    V: "...-",
+    W: ".--",
+    X: "-..-",
+    Y: "-.--",
+    Z: "--..",
+    1: ".----",
+    2: "..---",
+    3: "...--",
+    4: "....-",
+    5: ".....",
+    6: "-....",
+    7: "--...",
+    8: "---..",
+    9: "----.",
+    0: "-----",
+    ",": "--..--",
+    ".": ".-.-.-",
+    "?": "..--..",
+    ":": "---...",
+    "-": "-....-",
+    "@": ".--.-.",
+  };
+
+  const reverse = {};
+  Object.entries(obj).forEach(([key, value]) => {
+    reverse[value] = key;
+  });
+
+  const result = data[1]
+    .split(" ")
+    .map((el) => reverse[el])
+    .join("");
+  console.log(result);
+}
+
+solution(input);
