641b5a2cebae608428964e6ee0477edd7fba1cd0
이진호 <117588482+zynoobb@users.noreply.github.com>
2025-03-23 22:46:26 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 96 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "입력.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+function solution(data) {
+  const n = +data[0];
+
+  console.log(Math.ceil(n / 5));
+}
+
+solution(input);
